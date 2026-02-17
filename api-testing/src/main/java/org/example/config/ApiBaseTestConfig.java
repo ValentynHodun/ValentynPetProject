87920bdbe4a1724b608bb0c3c2f335801ba815cd
valentyn.hodun@gmail.com <valentyn.hodun@gmail.com>
2026-02-17 19:17:16 +0100

    lombok update

--- api-testing/src/main/java/org/example/config/ApiBaseTestConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example.config;
-
-import org.springframework.context.annotation.*;
-
-@Configuration
-@PropertySource({"classpath:environment/${target}/application.properties"})
-public class ApiBaseTestConfig {
-
-}
--- api-testing/src/main/java/org/example/models/WeatherResponse.java
+++
@@ -1,9 +0,0 @@
-package org.example.models;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class WeatherResponse {
-    // todo -> can be additionally implemented
-}
-
-
